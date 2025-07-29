c91b23980bd3c7ca84305d5ee02a9d84a8a55fbb
¨alex <fperdomo161dev@gmail.com>
2025-07-29 15:42:16 -0500

    se agrego clase mail y se modifico clase cliente

--- src/Hotel/Cliente.java
+++ src/Hotel/Cliente.java
@@ -8,6 +8,36 @@ package Hotel;
  * @author User
  */
 public class Cliente {
-    
+ //Atributo para agregar nombre    
  private String nombre;
+ //Atributo para agregar cedula
+ private String cedula;
+ //constructor que recibe nombre y cedula
+ public Cliente (String nombre, String cedula) {
+     this.nombre = nombre;
+     this.cedula = cedula;
+ }
+ //metodos
+ public String getnombre (){
+     return nombre;
+ } 
+ 
+ public void setnombre (String nombre){
+     this.nombre =nombre;
+ }
+  
+ public String getcedula (){
+     return cedula;
+ }
+ 
+ public void setcedula(String cedula) {
+     this.cedula = cedula;
+ }
+ 
+ public void mostrarInformacionCliente() {
+     
+     System.out.println("cliente:" +nombre+ "- cedula:" +cedula);
+ }
+ 
+ 
 }
